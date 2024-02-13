d0507dddbe745c8f90ec67d165122b3cf53d6dfd
Alireza <103531312+alirezashon@users.noreply.github.com>
2024-02-14 00:10:03 +0330

    Create data.ts

---
+++ food/data.ts
@@ -0,0 +1,199 @@
+interface Food {
+  name: string
+  price: number
+  src: string
+  quantity: number
+}
+export const foods: Food[][] | [[], [], [], [], [], [], []] = [
+    [],
+    [],
+    [
+    {
+      name: "کباب کوبیده",
+      price: 150000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "خورشت قیمه",
+      price: 120000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "جوجه کباب",
+      price: 180000,
+      src: "https://picsum.photos/200",
+      quantity: 4,
+    },
+    {
+      name: "قیمه بادمجان",
+      price: 140000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "مرغ ترکی",
+      price: 160000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "کتلت",
+      price: 100000,
+      src: "https://picsum.photos/200",
+      quantity: 5,
+    },
+    {
+      name: "کوکو سبزی",
+      price: 90000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "باقالی پلو با مرغ",
+      price: 200000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "آبگوشت",
+      price: 180000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "کله پاچه",
+      price: 220000,
+      src: "https://picsum.photos/200",
+      quantity: 1,
+    },
+  ],
+ [],
+  [],
+  [
+    {
+      name: "کباب کوبیده",
+      price: 150000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "خورشت قیمه",
+      price: 120000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "جوجه کباب",
+      price: 180000,
+      src: "https://picsum.photos/200",
+      quantity: 4,
+    },
+    {
+      name: "قیمه بادمجان",
+      price: 140000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "مرغ ترکی",
+      price: 160000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "کتلت",
+      price: 100000,
+      src: "https://picsum.photos/200",
+      quantity: 5,
+    },
+    {
+      name: "کوکو سبزی",
+      price: 90000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "باقالی پلو با مرغ",
+      price: 200000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "آبگوشت",
+      price: 180000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "کله پاچه",
+      price: 220000,
+      src: "https://picsum.photos/200",
+      quantity: 1,
+    },
+  ],
+ 
+  [
+    {
+      name: " کوبیده",
+      price: 150000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "خورشت قیمه",
+      price: 120000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "جوجه کباب",
+      price: 180000,
+      src: "https://picsum.photos/200",
+      quantity: 4,
+    },
+    {
+      name: "قیمه بادمجان",
+      price: 140000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "مرغ ترکی",
+      price: 160000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "کتلت",
+      price: 100000,
+      src: "https://picsum.photos/200",
+      quantity: 5,
+    },
+    {
+      name: "کوکو سبزی",
+      price: 90000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "باقالی پلو با مرغ",
+      price: 200000,
+      src: "https://picsum.photos/200",
+      quantity: 2,
+    },
+    {
+      name: "آبگوشت",
+      price: 180000,
+      src: "https://picsum.photos/200",
+      quantity: 3,
+    },
+    {
+      name: "کله پاچه",
+      price: 220000,
+      src: "https://picsum.photos/200",
+      quantity: 1,
+    },
+  ],
+]

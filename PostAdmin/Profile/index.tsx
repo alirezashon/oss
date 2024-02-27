b7a850fc2093d9a71b5507c3482dfecb4b02e8d1
Alireza <103531312+alirezashon@users.noreply.github.com>
2024-02-27 23:27:06 +0330

    Add files via upload

--- PostAdmin/Profile/index.tsx
+++ PostAdmin/Profile/index.tsx
@@ -1,52 +1,57 @@
-import Image from "next/image"
+import Items from "./Items/index"
+import Image from "next/Image"
+import Address from "./Address/index"
+import Chat from "./Chat/index"
+import Notification from "./Notifications/index"
 import styles from "./index.module.css"
+import { useState } from "react"
+import Orders from "./Orders/index"
+import Favorites from "./Favorites/index"
+import LastSeen from "./LastSeen/index"
 const Profile = () => {
+  const [state, setState] = useState()
+  const icons = [
+    {
+      tag: <Orders />,
+      name: "سفارشات",
+    },
+    { tag: <Favorites />, name: "مورد علاقه" },
+    {
+      tag: <Chat />,
+      name: "گفتگو",
+    },
+    {
+      tag: <Address />,
+      name: "آدرس ها",
+    },
+    {
+      tag: <LastSeen />,
+      name: "بازدید های اخیر",
+    },
+    {
+      tag: <Notification />,
+      name: "پیغام ها",
+    },
+  ]
   return (
     <>
       <div className={styles.container}>
+        <Items />
         <div className={styles.topBox}>
-        <div className={styles.profileBox}>
-          <Image className={styles.image} src={"/images/logo.png"} width={111} height={111}/>
-          <p>Name</p>
-          <p>7/3/1380</p>
-        </div>
-
-        <div className={styles.detailBox}>
-          <div className={styles.detailRow}>
-            <label>آدرس :</label>
-            <p>
-               street onvari thi wsay in the alley or the valley kalley por
-               street onvari thi wsay in the alley or the valley kalley por
-               street onvari thi wsay in the alley or the valley kalley por
-               street onvari thi wsay in the alley or the valley kalley por
-               street onvari thi wsay in the alley or the valley kalley por
-               street onvari thi wsay in the alley or the valley kalley por
-               street onvari thi wsay in the alley or the valley kalley por
-               street onvari thi wsay in the alley or the valley kalley por
-              gozalansovvichkonixy
-            </p>
+          <div className={styles.profileBox}>
+            <Image
+              className={styles.image}
+              src={"/images/logo.png"}
+              width={111}
+              height={111}
+            />
+            <p>Name</p>
+            <p>7/3/1380</p>
           </div>
-          <div className={styles.detailRow}>
-            <label>کد پستی :</label>
-            <p>75895862</p>
-          </div>
-          <div className={styles.detailRow}>
-            <label>شماره تلفن :</label>
-            <p> 865514</p>
-          </div>
-        <div className={styles.detailRow}>
-          <label>ایمیل</label>
-          <p>
-            street@sfg.cas
-          </p>
         </div>
-        </div>
-
         <div className={styles.orderHistory}>
-          <div className={styles.orderBox}>
-            
-          </div>
-        </div></div>
+          <div className={styles.orderBox}>kaktoz</div>
+        </div>
       </div>
     </>
   )
